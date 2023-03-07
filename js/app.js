60a2fc46ae17bea6ee24c884e5403b576d0660e8
Jair Chavez Islas <chamaco_03@hotmail.es>
2023-03-06 20:45:16 -0600

    Finalziamos con el codigo de la ultima clase

--- js/app.js
+++ js/app.js
@@ -1,40 +1,52 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
-let topHundred = [];
+const API = 'https://spotify81.p.rapidapi.com/top_200_tracks'
+const btnBuscar = document.querySelector('#buscador')
+let topTwoHundred = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadMusicData();
+  loadMusicData()
 })
 
-
 const loadMusicData = () => {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '4d425b0d96mshbed6647e5305b91p14d902jsn73e2a1a73e94',
-            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
-        .then(response => response.json())
-        .then(response => {
-            topHundred = response
-            console.log('Canciones: ', topHundred)
-            creaCards()
-        })
-        .catch(err => console.error(err));
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '747be6c6e0msh7e2183218542cbbp1e4682jsncb33cbcc42c3',
+      'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+    }
+  }
+  fetch(API, options)
+    .then(response => response.json())
+    .then(response => {
+      topTwoHundred = response
+      console.log('Canciones', topTwoHundred)
+      creaCards()
+    })
+    .catch(err => console.error(err))
 }
 
 const creaCards = () => {
-    topHundred.forEach((song) => {
-        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-        const clone = cardTop.cloneNode(true)
-        fragment.appendChild(clone)
-    });
-    contenido.appendChild(fragment)
+    topTwoHundred.forEach((song) => {
+      cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
+      cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+      let artists = ''
+      let size = song.trackMetadata.artists.length
+      song.trackMetadata.artists.forEach((item, index) => {
+        if (index === size-1){
+          artists += item.name
+        }
+        artists += item.name + '/'
+      })
+      cardTop.querySelector('.artistname').textContent = artists
+
+      const clone = cardTop.cloneNode(true)
+      fragment.appendChild(clone)
+  })
+  contenido.appendChild(fragment)
 }
 
-
+btnBuscar.addEventListener('keypress', () => {
+  console.log(btnBuscar.value)
+})
